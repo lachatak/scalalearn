0fd493885dfff5fafbaaa23ebc1187189415751a
klachata <klachata@gumtree.com>
2017-07-29 18:20:47 +0100

    add nginx

--- akka-persistence-sharding/performance-tests/src/test/scala/com/gumtree/pricing/performance/testing/ServerPerformanceTest.scala
+++ akka-persistence-sharding/performance-tests/src/test/scala/com/gumtree/pricing/performance/testing/ServerPerformanceTest.scala
@@ -6,39 +6,34 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.netty.handler.codec.http.HttpResponseStatus
 
-import scala.util.Random
-
 class ServerPerformanceTest extends Simulation {
-
-  val ips = List("8080", "8081", "8082", "8083")
 
   // Default configuration
   val httpProtocol = http.baseURL("http://127.0.0.1").disableWarmUp
 
-  val id = new AtomicInteger(300)
+  val id = new AtomicInteger(2000)
 
   val feeder = Iterator.continually(Map("id" -> id.getAndIncrement()))
-  val portfeeder = Iterator.continually(Map("port" -> Random.shuffle(ips).head))
 
   // Request URL details
-  val token = ":${port}/token/${id}"
-  val execute = ":${port}/execute/${id}"
+  val token = "/token/${id}"
+  val execute = "/execute/${id}"
 
   val serverScenario = scenario("Server Test")
     .feed(feeder)
-    .feed(portfeeder)
     .exec(http("token")
       .get(token)
-      .check(status.is(HttpResponseStatus.OK.code())))
-    .feed(portfeeder)
-    .exec(http("exec")
-      .get(execute)
-      .check(status.is(HttpResponseStatus.OK.code())))
+      .check(status.is(HttpResponseStatus.OK.code()), status.saveAs("status")))
+    .doIf(session => session("status").as[Int] == HttpResponseStatus.OK.code()) {
+      exec(http("execute")
+        .get(execute)
+        .check(status.is(HttpResponseStatus.OK.code())))
+    }
 
   val num = 100
 
   // Simulation set-up
-  setUp(serverScenario.inject(rampUsers(num * 3) over (num)))
+  setUp(serverScenario.inject(rampUsers(num * 4) over (num)))
     .protocols(httpProtocol)
     .assertions(
       global.failedRequests.percent.lt(1),

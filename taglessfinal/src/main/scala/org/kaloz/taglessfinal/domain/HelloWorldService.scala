bd05ffc916a837e6a646ed96f9bc5c3f9059e764
klachata <klachata@gumtree.com>
2017-12-29 18:45:16 +0000

    tagless final split domain infra

--- taglessfinal/src/main/scala/org/kaloz/taglessfinal/domain/HelloWorldService.scala
+++ taglessfinal/src/main/scala/org/kaloz/taglessfinal/domain/HelloWorldService.scala
@@ -4,12 +4,11 @@ import cats.MonadError
 
 case class HelloWorldService[F[_]](implicit M: MonadError[F, DomainError]) {
 
-  def hello(name: Name): F[Greeting] = {
+  def hello(name: Name): F[Greeting] =
     if (name.name.equals("krs")) {
       M.raiseError(NotSupportedName(name.name))
     } else {
       M.pure(Greeting(name.name))
     }
-  }
 
 }
--- taglessfinal/src/main/scala/org/kaloz/taglessfinal/domain/package.scala
+++ taglessfinal/src/main/scala/org/kaloz/taglessfinal/domain/package.scala
@@ -6,13 +6,13 @@ import org.scalameta.data.data
 
 package object domain {
 
-  abstract class DomainError(val message: String)
+  sealed trait Domain
+
+  sealed abstract class DomainError(val message: String) extends Domain
 
   case class InvalidName(name: String) extends DomainError(s"'$name' is not valid! Can contain only ${Name.ValidName}")
 
   case class NotSupportedName(name: String) extends DomainError(s"'$name' is not supported!")
-
-  trait Domain
 
   @data
   class Name private(val name: String) extends Domain
@@ -29,6 +29,6 @@ package object domain {
       }
   }
 
-  final case class Greeting(greeting: String) extends Domain
+  case class Greeting(greeting: String) extends Domain
 
 }
